ff45a57120fe1c8f9d7d1600c17e7e41ee4388f3
MarcosKaleu <marcos.kaleu@sempreceub.com>
2018-05-23 14:47:41 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TelaMarceloComponent } from './tela-marcelo/tela-marcelo.component';
 import { NovaTelaComponent } from './nova-tela/nova-tela.component';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { Marcelo2Component } from './marcelo2/marcelo2.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tela-marcelo', pathMatch: 'full' },
   { path: 'tela-marcelo', component: TelaMarceloComponent },
   { path: 'nova-tela', component: NovaTelaComponent },
-  { path: 'login', component: TelaLoginComponent }
+  { path: 'login', component: TelaLoginComponent },
+  { path: 'marcelo2', component: Marcelo2Component }
 ];
 
 @NgModule({
